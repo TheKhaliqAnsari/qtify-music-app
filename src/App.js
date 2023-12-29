281a01efffac5f2d9fa58e6296ccfe2a20513c5f
TheKhaliqAnsari <khaliqansari771@gmail.com>
2023-12-29 16:34:36 +0530

    Hero Section completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
+import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 
 function App() {
-  return <Navbar/>
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
 export default App;
